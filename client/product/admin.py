ea72062cd69e816e4ce1eac4f73fce44699a813c
Harun <harun1393@gmail.com>
2019-12-04 13:55:08 +0600

    date editing off in admin

--- client/product/admin.py
+++ client/product/admin.py
@@ -5,7 +5,6 @@ from .models import ProductQue
 class QueAdmin(admin.ModelAdmin):
     list_display = ("pk", "date", "is_resolved", "status")
     list_display_links = ("pk", )
-    list_editable = ("date", )
 
 
 admin.site.register(ProductQue, QueAdmin)
